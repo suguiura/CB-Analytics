faf38c7841cc6da73004b1fe0e34efd94121de31
Rafael S. Suguiura <rafael.suguiura@gmail.com>
2011-10-27 19:00:15 -0200

    Creating db section in config.yaml

--- script/connection.rb
+++ script/connection.rb
@@ -4,8 +4,8 @@ require 'rubygems'
 require 'active_record'
 
 config = YAML::load_file 'config.yaml'
-current = config['current']
+current = config['db']['current']
 
 #ActiveRecord::Base.logger = Logger.new(STDOUT)
-ActiveRecord::Base.establish_connection config[current]
+ActiveRecord::Base.establish_connection config['db'][current]
 
